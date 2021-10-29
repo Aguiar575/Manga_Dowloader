25b6f5b9e12d5c365e8c0112f86235a446be1ad7
Aguiar575 <aguiarus99arthurus@gmail.com>
2021-10-29 00:21:11 -0300

    Changes in logic; function getMangaByUrl returning list of pages chapter. ONLY SPECIFIC SITE IN THIS MOMENT. URL: https://manganato.com

--- manga_crawler/lib/manga_crawler.ex
+++ manga_crawler/lib/manga_crawler.ex
@@ -1,24 +1,29 @@
 defmodule MangaCrawler do
 
   use HTTPoison.Base
-
   @moduledoc """
   Documentation for `MangaCrawler`.
   """
 
   @doc """
-  Hello world.
+    Get manga from url.
 
-  ## Examples
+    ## Examples
 
-      iex> MangaCrawler.hello()
-      :world
+        iex> MangaCrawler.getMangaByUrl("http://www.mangareader.net/naruto/")
+        :Naruto
+    """
+  def getMangaByUrl(url) do
+    url
+    |> requestSite()
+    |> getMangaList()
+    |> getMangaPagesFromList()
+  end
 
-  """
-  def requestSite(url) do
+  defp requestSite(url) do
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-         getMangaList(body)
+         body
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts "Not found :("
       {:error, %HTTPoison.Error{reason: reason}} ->
@@ -26,7 +31,16 @@ defmodule MangaCrawler do
     end
   end
 
-  def getMangaList(body) do
-    IO.puts Floki.find(body, "div.container-chapter-reader")
+  defp getMangaList(body) do
+    body
+    |> Floki.find("div.container-chapter-reader img")
   end
+
+  defp getMangaPagesFromList(items) do
+    items
+    |> Floki.attribute("src")
+  end
+
 end
+
+#"https://readmanganato.com/manga-aa951409/chapter-1" |> MangaCrawler.requestSite
